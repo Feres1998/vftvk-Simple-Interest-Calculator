2c8ed1f9dfb46d84b42e78d214eb47dda211cd89
Feres1998 <91794519+Feres1998@users.noreply.github.com>
2021-10-02 19:37:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal *years * rate /100;
     var year = new Date().getFullYear() + parseInt(years);
+    var result = document.getElementById("result").innerHTML ="if you deposite"+ principal+"at an interest rate of "+rate+"You will receive an amount of "+ interest+ "in the year "+year;
 }
 
 function updateRate(){
         var rateval = document.getElementById("rate").value;
-        document.getElementById("rate_val").innerText = rateval;
+        document.getElementById("rate_val").innerHTML = rateval;
 }
+
